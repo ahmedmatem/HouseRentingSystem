f19fd60ed70f7fb573fa642c138893d60e789d45
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-02-24 19:15:42 +0200

    HouseConfiguration addedd

---
+++ HouseRentingSystem.Data/EntityTypesConfigurations/HouseConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using HouseRentingSystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Data.EntityTypesConfigurations
+{
+    public class HouseConfiguration : IEntityTypeConfiguration<House>
+    {
+        public void Configure(EntityTypeBuilder<House> builder)
+        {
+            builder.HasOne(h => h.Category)
+                .WithMany(c => c.Houses)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(h => h.Agent)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
